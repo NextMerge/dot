d54a916baffac1fe50d37b550f9ecdc7e530110b
Mark Jarjour <mj_git_signing@sent.com>
2025-02-02 13:59:27 -0500

    Port Vivaldi patch tool to fish, Vivaldi mod now stops thumbnail updates when navigating to another tab

--- vivaldi/custom.js
+++ vivaldi/custom.js
@@ -1,12 +1,12 @@
 (function () {
     let timer = null;
 
+    // Tab URL changes -> capture the tab after a delay and update Vivaldi's thumbnail
     chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
         if (changeInfo.url && timer === null) {
             timer = setTimeout(async () => {
                 try {
-                    const window = await chrome.windows.getCurrent();
-                    const tabCapture = await chrome.tabs.captureVisibleTab(window.id);
+                    const tabCapture = await chrome.tabs.captureVisibleTab(tab.windowId);
 
                     const vivExtData = JSON.parse(tab.vivExtData);
                     vivExtData.thumbnail = tabCapture;
@@ -19,7 +19,16 @@
                 } finally {
                     timer = null;
                 }
-            }, chrome.tabs.MAX_CAPTURE_VISIBLE_TAB_CALLS_PER_SECOND * 1000);
+            }, Math.max(1000 / chrome.tabs.MAX_CAPTURE_VISIBLE_TAB_CALLS_PER_SECOND, 2000));
         }
     });
+
+    // Switch tabs -> clear the timer
+    chrome.tabs.onActivated.addListener(() => {
+        if (timer !== null) {
+            clearTimeout(timer);
+            timer = null;
+        }
+    });
+
 })();
