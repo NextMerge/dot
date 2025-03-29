123c7cc1a5efe961689b127479aa51d38cc63759
Mark Jarjour <mj_git_signing@sent.com>
2025-03-29 13:46:30 -0400

    Add more binds to iwf

--- fish/functions/iwf.fish
+++ fish/functions/iwf.fish
@@ -1,7 +1,25 @@
 function iwf --description "I want file - Interactive file selection and preview using fzf"
-    set selected (__fd_without_protected --type f --type l | fzf -i --preview 'bat --style=numbers --color=always {}')
+    set selected (__fd_without_protected --type f --type l | fzf -i \
+        --preview 'bat --style=numbers --color=always {}' \
+        --header "C-y: Copy path, C-d: Go to dir, C-v: View file" \
+        --expect=ctrl-y,ctrl-d,ctrl-v)
+    
     if test -n "$selected"
-        bat "$selected"
+        set key (echo $selected | cut -d' ' -f1)
+        set file (echo $selected | cut -d' ' -f2-)
+        
+        switch $key
+            case ctrl-y
+                echo $file | pbcopy
+                echo "Path copied: $file"
+            case ctrl-d
+                cd (dirname $file)
+            case ctrl-v
+                bat $file
+            case '*'
+                cd (dirname $file)
+                bat $file
+        end
     end
 end
 
