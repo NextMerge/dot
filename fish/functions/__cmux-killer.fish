d0d83336df1d36d23468894811835aeabdfc273b
Mark Jarjour <mj_git_signing@sent.com>
2025-06-11 19:50:01 -0400

    completions update (orbstack update?), cmux manually kills the servers on exit to prevent lingering processes

--- fish/functions/__cmux-killer.fish
+++ fish/functions/__cmux-killer.fish
@@ -6,6 +6,10 @@ function check_docker
         osascript -e 'display notification "Docker containers are not running. Killing cmux session." with title "Docker Alert"'
         sleep 5
         if not docker ps --filter "name=sombra-db-1" --format "{{.Status}}" 2>/dev/null | grep -q "Up"
+            # Send Ctrl-C to both top panes, wait, then kill session
+            tmux send-keys -t {top-left} C-c
+            tmux send-keys -t {top-right} C-c
+            sleep 4
             tmux kill-session
             exit 1
         end
