107badcb0ebf2c5413d5d0b34aeb5232e4eddc6b
Mark Jarjour <mj_git_signing@sent.com>
2025-09-23 18:10:07 -0400

    Maybe fixing bug someone reported with Vivaldi script

--- vivaldi/custom.js
+++ vivaldi/custom.js
@@ -1,34 +1,43 @@
+// biome-ignore lint/complexity/useArrowFunction: nah
 (function () {
-    let timer = null;
+  let timer = null;
 
-    // Tab URL changes -> capture the tab after a delay and update Vivaldi's thumbnail
-    chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-        if (changeInfo.url && timer === null && tab.active) {
-            timer = setTimeout(async () => {
-                try {
-                    const tabCapture = await chrome.tabs.captureVisibleTab(tab.windowId);
+  // Tab URL changes -> capture the tab after a delay and update Vivaldi's thumbnail
+  chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.url && timer === null && tab.active) {
+      timer = setTimeout(
+        async () => {
+          try {
+            const thatTab = await chrome.tabs.get(tabId);
+            const tabCapture = await chrome.tabs.captureVisibleTab(
+              thatTab.windowId,
+            );
 
-                    const vivExtData = JSON.parse(tab.vivExtData);
-                    vivExtData.thumbnail = tabCapture;
+            const vivExtData = JSON.parse(thatTab.vivExtData);
+            vivExtData.thumbnail = tabCapture;
 
-                    await chrome.tabs.update(tabId, {
-                        vivExtData: JSON.stringify(vivExtData),
-                    });
-                } catch (error) {
-                    console.error(error);
-                } finally {
-                    timer = null;
-                }
-            }, Math.max(1000 / chrome.tabs.MAX_CAPTURE_VISIBLE_TAB_CALLS_PER_SECOND, 2000));
-        }
-    });
-
-    // Switch tabs -> clear the timer
-    chrome.tabs.onActivated.addListener(() => {
-        if (timer !== null) {
-            clearTimeout(timer);
+            await chrome.tabs.update(tabId, {
+              vivExtData: JSON.stringify(vivExtData),
+            });
+          } catch (error) {
+            console.error(error);
+          } finally {
             timer = null;
-        }
-    });
+          }
+        },
+        Math.max(
+          1000 / chrome.tabs.MAX_CAPTURE_VISIBLE_TAB_CALLS_PER_SECOND,
+          2000,
+        ),
+      );
+    }
+  });
 
+  // Switch tabs -> clear the timer
+  chrome.tabs.onActivated.addListener(() => {
+    if (timer !== null) {
+      clearTimeout(timer);
+      timer = null;
+    }
+  });
 })();
